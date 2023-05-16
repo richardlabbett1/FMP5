1913d91b8c9f469d4178b830c4502e178af38ef8
richardlabbett1 <512636@chrome.escg.ac.uk>
2023-05-16 19:37:37 +0100

    Enemy Ai working kinda

--- Assets/StealthEnemyAI.cs
+++ Assets/StealthEnemyAI.cs
@@ -3,126 +3,102 @@ using UnityEngine.AI;
 
 public class StealthEnemyAI : MonoBehaviour
 {
-    public Transform[] waypoints;       // Array of patrol waypoints
-    public Transform player;            // Reference to the player object
-    public float fieldOfViewAngle = 90f; // AI's field of view angle
-    public float detectionTime = 2f;     // Time taken to detect the player
-    public float chaseSpeed = 3.5f;      // AI's speed when chasing the player
-    public float patrolSpeed = 2f;       // AI's patrol speed
-    public float attackRange = 1.5f;     // Range for attacking the player
+    public Transform[] waypoints;   // List of waypoints for the AI to patrol
+    public float fieldOfView = 90f; // Field of view angle for the AI
+    public float detectionTime = 2f; // Time taken by the AI to detect the player
+    public float speed = 3.5f;      // Movement speed of the AI
 
-    private int currentWaypointIndex = 0;    // Current waypoint index
-    private NavMeshAgent agent;              // Reference to the NavMeshAgent component
-    private bool playerDetected = false;     // Flag indicating if player is detected
-    private Vector3 lastKnownPlayerPosition; // Last known player position
-    private float detectionTimer = 0f;       // Timer for detection time
+    private NavMeshAgent agent;
+    private int currentWaypointIndex;
+    private Transform player;
+    private Vector3 lastKnownPosition;
+    private bool playerDetected;
+    private float detectionTimer;
 
-    void Start()
+    private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        currentWaypointIndex = 0;
+        lastKnownPosition = transform.position;
+        playerDetected = false;
+        detectionTimer = 0f;
 
-        // Set initial destination to the first waypoint
-        agent.SetDestination(waypoints[currentWaypointIndex].position);
+        PatrolToNextWaypoint();
     }
 
-    void Update()
+    private void Update()
     {
-        // If the player is detected, chase the player
-        if (playerDetected)
+        if (!playerDetected)
         {
-            ChasePlayer();
+            if (CanSeePlayer())
+            {
+                detectionTimer += Time.deltaTime;
+                if (detectionTimer >= detectionTime)
+                {
+                    // Player detected
+                    playerDetected = true;
+                    detectionTimer = 0f;
+                    lastKnownPosition = player.position;
+                }
+            }
+            else
+            {
+                detectionTimer = 0f;
+            }
+
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                PatrolToNextWaypoint();
+            }
         }
         else
         {
-            // Otherwise, patrol between waypoints
-            Patrol();
+            // Move towards the player's last known position
+            agent.SetDestination(lastKnownPosition);
+
+            if (CanSeePlayer())
+            {
+                // Player still visible, update last known position
+                lastKnownPosition = player.position;
+            }
+            else
+            {
+                detectionTimer += Time.deltaTime;
+                if (detectionTimer >= detectionTime)
+                {
+                    // Player lost
+                    playerDetected = false;
+                    detectionTimer = 0f;
+                    PatrolToNextWaypoint();
+                }
+            }
         }
     }
 
-    void Patrol()
+    private void PatrolToNextWaypoint()
     {
-        // If the AI is close enough to the current waypoint, move to the next waypoint
-        if (agent.remainingDistance < agent.stoppingDistance)
-        {
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
-            agent.SetDestination(waypoints[currentWaypointIndex].position);
-        }
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
     }
 
-    void ChasePlayer()
+    private bool CanSeePlayer()
     {
-        // If the player is in the AI's field of view
         Vector3 directionToPlayer = player.position - transform.position;
-        if (Vector3.Angle(transform.forward, directionToPlayer) < fieldOfViewAngle * 0.5f)
+        float angle = Vector3.Angle(transform.forward, directionToPlayer);
+
+        if (angle <= fieldOfView * 0.5f)
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, directionToPlayer, out hit))
             {
-                if (hit.collider.gameObject == player.gameObject)
+                if (hit.collider.CompareTag("Player"))
                 {
-                    // Player is in sight
-                    lastKnownPlayerPosition = player.position;
-                    agent.SetDestination(player.position);
-                    agent.speed = chaseSpeed;
-
-                    // If the AI is within attacking range, attack the player
-                    if (Vector3.Distance(transform.position, player.position) <= attackRange)
-                    {
-                        AttackPlayer();
-                    }
-                    return;
+                    return true;
                 }
             }
         }
 
-        // Player is not in sight, go to the last known position
-        agent.SetDestination(lastKnownPlayerPosition);
-        agent.speed = patrolSpeed;
-
-        // If the AI reaches the last known position, stop chasing
-        if (agent.remainingDistance < agent.stoppingDistance)
-        {
-            playerDetected = false;
-        }
-    }
-
-    void AttackPlayer()
-    {
-        // Implement your attack behavior here
-        Debug.Log("Attacking player!");
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        // If the player enters the AI's trigger zone, start detection process
-        if (other.gameObject == player.gameObject)
-        {
-            detectionTimer = detectionTime;
-        }
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject == player.gameObject)
-        {
-            Vector3 directionToPlayer = other.transform.position - transform.position;
-            float angle = Vector3.Angle(transform.forward, directionToPlayer); if (angle < fieldOfViewAngle * 0.5f)
-            {
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, directionToPlayer, out hit))
-                {
-                    if (hit.collider.gameObject == player.gameObject)
-                    {
-                        // Start detection process
-                        detectionTimer -= Time.deltaTime;
-                        if (detectionTimer <= 0f)
-                        {
-                            playerDetected = true;
-                            agent.speed = chaseSpeed;
-                        }
-                    }
-                }
-            }
-        }
+        return false;
     }
 }
